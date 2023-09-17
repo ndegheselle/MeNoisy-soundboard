1100caaa2e28c5f3c02ba9af877de1711f93253e
Nicolas de gheselle <nicolas@degheselle.fr>
2023-09-17 21:12:05 +0200

    Add a FilePicker component

---
+++ MeNoisy-soundboard.App/Base/Components/FilePicker.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeNoisy_soundboard.App.Base.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour FilePicker.xaml
+    /// </summary>
+    public partial class FilePicker : UserControl
+    {
+
+        #region Dependency properties
+
+        public static readonly DependencyProperty SelectedFilePathProperty = DependencyProperty.Register(
+            "SelectedFilePath", typeof(string), typeof(FilePicker));
+
+        public string SelectedFilePath
+        {
+            get => (string)GetValue(SelectedFilePathProperty);
+            set => SetValue(SelectedFilePathProperty, value);
+        }
+
+        #endregion
+
+        public FilePicker()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            InputFilePath.Text = "";
+        }
+
+        private void OpenPicker_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                InputFilePath.Text = openFileDialog.FileName;
+        }
+    }
+}
