7983eaac86a391c65d0e54f08f9737a0dd89574c
Nicolas de gheselle <nicolas@degheselle.fr>
2023-10-04 19:38:27 +0200

    Added sound progress

--- MeNoisy-soundboard.App/Logic/Sounds/AudioPlayer.cs
+++ MeNoisy-soundboard.App/Logic/Sounds/AudioPlayer.cs
@@ -3,10 +3,7 @@ using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MeNoisySoundboard.App.Logic.Sounds
 {
@@ -18,22 +15,22 @@ namespace MeNoisySoundboard.App.Logic.Sounds
         public bool IsPlaying { get; set; } = false;
 
         private Sound sound;
+
+        private Timer timer;
         private WaveOutEvent waveOutDevice;
         private AudioFileReader audioFileReader;
 
-        public TimeSpan TotalTime
-        {
-            get
-            {
-                return audioFileReader.TotalTime;
-            }
-        }
+        public TimeSpan TotalTime { get; private set; }
+        public TimeSpan CurrentTime { get; private set; }
 
         public AudioPlayer(Sound _sound)
         {
             sound = _sound;
             audioFileReader = new AudioFileReader(sound.FilePath);
             audioFileReader.Volume = sound.Volume;
+
+            CurrentTime = audioFileReader.CurrentTime;
+            TotalTime = audioFileReader.TotalTime;
         }
 
         public void Play()
@@ -43,15 +40,30 @@ namespace MeNoisySoundboard.App.Logic.Sounds
                 waveOutDevice = new WaveOutEvent();
                 waveOutDevice.Init(audioFileReader);
                 waveOutDevice.PlaybackStopped += WaveOutDevice_PlaybackStopped;
+
+                timer = new Timer();
+                timer.Tick += new EventHandler(UpdateCurrentTime);
+                timer.Interval = 100;
+                timer.Start();
             }
 
             waveOutDevice.Play();
             IsPlaying = true;
         }
 
+        // Bubble up
         private void WaveOutDevice_PlaybackStopped(object? sender, StoppedEventArgs e)
         {
+            timer = null;
             FinishedEvent.Invoke();
+        }
+
+        private void UpdateCurrentTime(object? sender, EventArgs e)
+        {
+            if (IsPlaying)
+            {
+                CurrentTime = audioFileReader.CurrentTime;
+            }
         }
 
         public void Pause()
@@ -68,6 +80,7 @@ namespace MeNoisySoundboard.App.Logic.Sounds
 
         public void Dispose()
         {
+            IsPlaying = false;
             FinishedEvent = null;
             waveOutDevice.Dispose();
             audioFileReader.Dispose();
