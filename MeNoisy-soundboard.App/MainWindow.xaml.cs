8ca603425dbebe6f18b80b550aca775bc4ca2002
Nicolas de gheselle <nicolas@degheselle.fr>
2023-10-18 23:46:04 +0200

    Device selection on the Webview2

--- MeNoisy-soundboard.App/MainWindow.xaml.cs
+++ MeNoisy-soundboard.App/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using AdonisUI.Controls;
 using MeNoisySoundboard.App.Base.UI;
+using MeNoisySoundboard.App.Contexts;
 using MeNoisySoundboard.App.Logic;
 using MeNoisySoundboard.App.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -26,6 +28,7 @@ namespace MeNoisySoundboard.App
             Navigation = new NavigationHandler(MainContainer);
             Navigation.Navigate(new HomePage());
 
+
             Init();
         }
 
@@ -33,13 +36,70 @@ namespace MeNoisySoundboard.App
         {
             await webView.EnsureCoreWebView2Async();
             webView.CoreWebView2.OpenDevToolsWindow();
-            await webView.CoreWebView2.Profile.SetPermissionStateAsync(Microsoft.Web.WebView2.Core.CoreWebView2PermissionKind.OtherSensors, "https://www.microsoft.com", Microsoft.Web.WebView2.Core.CoreWebView2PermissionState.Allow);
-            webView.CoreWebView2.PermissionRequested += CoreWebView2_PermissionRequested;
+            await webView.CoreWebView2.Profile.SetPermissionStateAsync(
+                Microsoft.Web.WebView2.Core.CoreWebView2PermissionKind.Microphone,
+                "https://www.youtube.com/watch?v=6nlsdu6N5d8", 
+                Microsoft.Web.WebView2.Core.CoreWebView2PermissionState.Allow);
+            await webView.CoreWebView2.Profile.SetPermissionStateAsync(
+                Microsoft.Web.WebView2.Core.CoreWebView2PermissionKind.Autoplay,
+                "https://www.youtube.com/watch?v=6nlsdu6N5d8",
+                Microsoft.Web.WebView2.Core.CoreWebView2PermissionState.Allow);
+            await InitWebViewJs();
+            await SetWebViewAudio(GlobalParamsProvider.Params.OutputDevice.Value);
         }
 
-        private void CoreWebView2_PermissionRequested(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2PermissionRequestedEventArgs e)
+        private async Task SetWebViewAudio(string value)
         {
-            throw new NotImplementedException();
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync($"setSelectedAudioDeviceId('{value}')");
+        }
+
+        public async Task InitWebViewJs()
+        {
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(@"
+window.audioOutputSelect = { deviceId: null, observer: null };
+async function setSelectedAudioDeviceId(deviceName) {
+    let devices = await navigator.mediaDevices.enumerateDevices()
+    devices = devices.filter(x => x.kind == 'audiooutput' && x.deviceId != 'communications');
+
+    console.log(devices, deviceName);
+    let selectedDevice = devices.find(x => x.label.includes(deviceName));
+
+    if (!selectedDevice) return;
+
+    window.audioOutputSelect.deviceId = selectedDevice.deviceId || 'default';
+}
+
+function applyAudioDeviceToAll()
+{
+    document.querySelectorAll('video').forEach(x => x.setSinkId(window.audioOutputSelect.deviceId));
+    document.querySelectorAll('audio').forEach(x => x.setSinkId(window.audioOutputSelect.deviceId));   
+}
+
+function mutationCallback(mutationsList, observer) {
+  mutationsList.forEach((mutation) => {
+    if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach((node) => {console.log(node);
+        if (node instanceof HTMLMediaElement) {
+          node.setSinkId(window.audioOutputSelect.deviceId);
+        }
+      });
+    }
+  });
+}
+
+window.addEventListener('load', () => {
+    if (!window.audioOutputSelect.observer) {
+console.log(window.audioOutputSelect.observer);
+    window.audioOutputSelect.observer = new MutationObserver(mutationCallback);
+    window.audioOutputSelect.observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+}
+
+
+});
+                ");
         }
 
         #region UI Events
